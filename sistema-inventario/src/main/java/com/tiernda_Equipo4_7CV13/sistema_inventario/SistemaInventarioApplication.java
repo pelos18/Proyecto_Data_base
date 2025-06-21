0a243906ef1788a935f3dd4d4f8cbef456d043a7
dona_ <dona_pan_lol@hotmail.com>
2025-06-21 06:09:18 -0600

    spring-sin conectar

---
+++ sistema-inventario/src/main/java/com/tiernda_Equipo4_7CV13/sistema_inventario/SistemaInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.tiernda_Equipo4_7CV13.sistema_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaInventarioApplication.class, args);
+	}
+
+}
---
+++ sistema-inventario/src/test/java/com/tienda_Equipo4_7CV13/sistema_inventario/SistemaInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiernda_Equipo4_7CV13.sistema_inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
